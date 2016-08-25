1e1164ebb8f51d4467a4fac022b69c1ac17c0cbe
Hriday Sharma <hridaysharma42@gmail.com>
2016-08-25 08:34:11 +0000

    msm8916-common: Add Vulkan Features
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -173,6 +173,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
+# Vulkan
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd_default.conf \
